2a023b3ede28ff9f1497a63831d91e0fd71e89e3
Liang Gao <lianggao@thoughtworks.com>
2017-04-11 17:17:25 +0800

    update travis configruation

--- test/run.sh
+++ test/run.sh
@@ -17,20 +17,4 @@ exit $ret
 fi
 rm -rf build
 
-cd ../initial
-
-mvn clean compile
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf target
-
-./gradlew compileJava
-ret=$?
-if [ $ret -ne 0 ]; then
-exit $ret
-fi
-rm -rf build
-
 exit
