e8ba8c4cc7114ef5e2337e2baa9b6c2e69366051
Liang Gao <lianggao@thoughtworks.com>
2017-04-23 19:43:35 +0800

    correct script code

--- scripts/jenkins/build.sh
+++ scripts/jenkins/build.sh
@@ -3,18 +3,19 @@ cd $(dirname $0)
 
 cd ../../
 
-DOCKER_IMAGE = "local/myapp:latest"
+DOCKER_IMAGE="local/myapp:latest"
 
+echo ">>>========= build docker image with latest source code"
 docker build -t $DOCKER_IMAGE ./
 
 mkdir -p build
 docker run -i -e DOCKER_HOST_USERID="$(id -u):$(id -g)" -v gradle-cache:/root/.gradle -v $(pwd)/build:/root/project/build --rm --name=myapp_build $DOCKER_IMAGE /bin/bash << "COMMANDS"
 set -o errexit -o nounset \
 \
-&& echo ">>>========= run build" \
+&& echo ">>>========= run build in container" \
 && rm -rf build/* \
 && ./gradlew build \
 \
-&& echo ">>>========= changing owner" \
+&& echo ">>>========= changing artifacts owner" \
 && chown --recursive $DOCKER_HOST_USERID build
 COMMANDS
--- scripts/jenkins/run.sh
+++ scripts/jenkins/run.sh
@@ -3,8 +3,10 @@ cd $(dirname $0)
 
 cd ../../
 
-DOCKER_IMAGE = "local/myapp:latest"
+DOCKER_IMAGE="local/myapp:latest"
 
+echo ">>>========= build docker image with latest source code"
 docker build -t $DOCKER_IMAGE ./
 
+echo ">>>========= run application in docker container"
 docker run -it -v gradle-cache:/root/.gradle -p 8080:8080 --rm --name=myapp_run $DOCKER_IMAGE ./gradlew bootrun
